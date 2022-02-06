5be82e0879ea82286ae139ab3c950f400247d4a8
auth00 <thomas@famaugustsson.se>
2022-02-06 09:44:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ywmo5e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ywmo5e)
---
+++ index.js
@@ -0,0 +1,20 @@
+// Import stylesheets
+import './style.scss';
+
+/*
+ * This script sets a variable called --scrollbar--width
+ * that always represents the width of
+ * the scrollbar if it is visible
+ */
+
+const innerWidthFiller = document.createElement('div');
+innerWidthFiller.style.cssText = 'position: fixed; left: 0; right: 0';
+document.body.appendChild(innerWidthFiller);
+
+const resizeObserver = new ResizeObserver(() => {
+  document.body.style.setProperty(
+    '--scrollbar--width',
+    window.innerWidth - innerWidthFiller.offsetWidth + 'px'
+  );
+});
+resizeObserver.observe(document.body);
